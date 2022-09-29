9eb224065d1edb8d821a29c82ad2b856871385c8
knyongki <yyongkisetiawan@gmail.com>
2022-09-29 22:36:33 +0700

    membuat web kalkulator

---
+++ assets/kalkulator.js
@@ -0,0 +1,110 @@
+const calculator = {
+    displayNumber: '0',
+    operator: null,
+    firstNumber: null,
+    isWaitForSecondNumber: false,
+}
+
+function updateDisplay() {
+    document.querySelector('#displayNumber').innerText = calculator.displayNumber;
+}
+
+function clearCalculator() {
+    calculator.displayNumber = '0';
+    calculator.operator = null;
+    calculator.firstNumber = null;
+    calculator.isWaitForSecondNumber = false;
+}
+
+function inputDigit(digit) {
+    if (calculator.displayNumber === '0') {
+        calculator.displayNumber = digit;
+    } else {
+        calculator.displayNumber += digit;
+    }
+}
+
+function inverseNumber() {
+    if (calculator.displayNumber === '0') {
+        return;
+    }
+    calculator.displayNumber = calculator.displayNumber * -1;
+}
+
+function hendleOperator(operator) {
+    if (!calculator.isWaitForSecondNumber) {
+        calculator.operator = operator;
+        calculator.isWaitForSecondNumber = true;
+        calculator.firstNumber = calculator.displayNumber;
+
+        //mengatur ulang nilai display number supaya tombol selanjutnya dimulai dari angka pertama lagi
+        calculator.displayNumber = '0';
+    } else {
+        alert('Operator sudah ditetapkan');
+    }
+}
+
+function performCalculation() {
+    if (calculator.firstNumber == null || calculator.operator == null) {
+        alert('Anda belum menetapkan operator');
+        return;
+    }
+
+    let result = 0;
+    if (calculator.operator === '+') {
+        result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
+    } else {
+        result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
+    }
+
+    //objek yang dikirim sebagai argumen put history
+    const history = {
+        firstNumber: calculator.firstNumber,
+        secondNumber: calculator.displayNumber,
+        operator: calculator.operator,
+        result: result
+    }
+
+    putHistory();
+    calculator.displayNumber = result;
+    renderHistory();
+}
+
+const buttons = document.querySelectorAll('.button')
+
+for (const button of buttons) {
+    button.addEventListener('click', function(event) {
+        //mendapatkan objek elemen yang diklik
+
+        const target = event.target;
+
+        if (target.classList.contains('clear')) {
+            clearCalculator();
+            updateDisplay();
+
+            return;
+        }
+
+        if (target.classList.contains('negative')) {
+            inverseNumber();
+            updateDisplay()
+            return;
+        }
+
+        if (target.classList.contains('equals')) {
+            performCalculation();
+            updateDisplay();
+
+            return;
+        }
+
+        if (target.classList.contains('operator')) {
+            hendleOperator(target.innerText);
+
+            return;
+        }
+
+        inputDigit(target.innerText);
+        updateDisplay();
+    });
+}
